f3c10e431141b7cc8159a79389c715f364880c2c
Denis Kaysarov <kaysik_it@mail.ru>
2022-08-14 16:36:23 +0500

    Register models in admin Add templates Add output movies Add output of movie descriptions

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Movie
+
+
+class MoviesView(ListView):
+    """Список фильмов"""
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    template_name = "movies/movies.html"
+
+
+class MovieDetailView(DetailView):
+    """Полное описание фильма"""
+    model = Movie
+    slug_field = "url"
