93f94dcf67155a80c60e4e4891215334fc750b60
Denis Kaysarov <kaysik_it@mail.ru>
2022-08-24 19:58:07 +0500

    Add creating simple pages

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.flatpages',
     'ckeditor',
     'ckeditor_uploader',
     'movies',
@@ -50,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -200,3 +203,5 @@ RECAPTCHA_PUBLIC_KEY = "6Lcs26AhAAAAAFlUR8t6iYANzvreNHFQTuYL2lft"
 RECAPTCHA_PRIVATE_KEY = "6Lcs26AhAAAAAOHqp4zLQgGlhLqocvKBs6nshlW-"
 RECAPTCHA_DEFAULT_ACTION = 'generic'
 RECAPTCHA_SCORE_THRESHOLD = 0.5
+
+SITE_ID = 1
